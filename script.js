4ce79170a42abab9868c11ee346e333bac0f17da
shubham kumar <65395298+Shubham800210@users.noreply.github.com>
2023-03-09 23:44:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 // complete the given function
 
 function palindrome(str){
-	var len = str.trim().toLowerCase().length;
-   for(var i=0;i<len/2;i++){
+	// var len = str.trim().toLowerCase().length;
+ //   for(var i=0;i<len/2;i++){
 
-	   if(str[i] !== str[len - 1 - i]){
-		   return false;
-	   }
-   }
-	return true;
+	//    if(str[i] !== str[len - 1 - i]){
+	// 	   return false;
+	//    }
+ //   }
+	// return true;
 	
-	// let a=str.trim().toLowerCase().length;
+	let a=str.trim().toLowerCase().length;
 	
-	//  // const len = str.length;
+	 // const len = str.length;
 
- //    for (let i = 0; i < a / 2; i++) {
+    for (let i = 0; i < a / 2; i++) {
 
        
- //        if (str[i] !== str[a - 1 - i]) {
- //            return true;
- //        }
- //    }
- //    return false;
+        if (str[i] !== str[a - 1 - i]) {
+            return true;
+        }
+    }
+    return false;
 }
 module.exports = palindrome
